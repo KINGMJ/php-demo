ce4c5a0900b0ab08f31c1f384a052f7f469a82c3
JerryMei <328047478@qq.com>
2019-09-04 11:09:33 +0800

    数组示例

--- application/controllers/Array_demo.php
+++ application/controllers/Array_demo.php
@@ -1,6 +1,5 @@
 <?php
 
-
 class Array_demo extends CI_Controller
 {
 	function __construct() {
@@ -21,6 +20,41 @@ class Array_demo extends CI_Controller
 			$item['age'] = '10';
 		});
 
+		print_r("<pre>");
 		print_r($arr);
+	}
+
+	public function test2() {
+		$arr = array(
+			0 =>
+				array(
+					'project_stat' => 'DemandProgress' ,
+					'unsupport_personal_version' => '1' ,
+				) ,
+			1 =>
+				array(
+					'project_stat' => 'TeamVelocity' ,
+					'unsupport_personal_version' => '0' ,
+				) ,
+			2 =>
+				array(
+					'project_stat' => 'DefectDistribution' ,
+					'unsupport_personal_version' => '1' ,
+				)
+		);
+		echo $this->find_key_by_value($arr , 'TeamVelocity');
+	}
+
+	/**二维数组根据键值返回键名
+	 * @param $arr
+	 * @param $str
+	 * @return bool|int|string
+	 */
+	private function find_key_by_value($arr , $str) {
+		foreach ($arr as $k => $v) {
+			if (in_array($str , $v))
+				return $k;
+		}
+		return FALSE;
 	}
 }
